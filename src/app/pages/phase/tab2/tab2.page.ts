74999a7ec610ed721e6cc31ae3b3f18890716378
Omar Lopez <omarlopch@gmail.com>
2020-01-17 15:25:10 -0700

    actualizado por omar

--- src/app/pages/phase/tab2/tab2.page.ts
+++ src/app/pages/phase/tab2/tab2.page.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
+  opcion;
+
+  busqueda;
+  fase = 'inicial';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  segmentChangedRegistros( event ) {
+    this.opcion = event.detail.value;
+    console.log(this.opcion);
+    switch( this.opcion ) {
+      case 'programa':
+        break;
+      case 'desempeno':
+        break;
+      default:
+        break;
+    }
+  }
+  eventListener( data: string ) {
+    this.busqueda = data[0];
+    this.fase = data[1];
+
+    console.log(this.busqueda, this.fase);
   }
 
 }
--- src/app/pages/physio/tab2/tab2.page.ts
+++ src/app/pages/physio/tab2/tab2.page.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
+  opcion;
 
+  busqueda;
+  fase = 'inicial';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  segmentChangedRegistros( event ) {
+    this.opcion = event.detail.value;
+    console.log(this.opcion);
+    switch( this.opcion ) {
+      case 'programaDiario':
+        break;
+      case 'reporteDesempeno':
+        break;
+      default:
+        break;
+    }
+  }
+  eventListener( data: string ) {
+    this.busqueda = data[0];
+    this.fase = data[1];
+
+    console.log(this.busqueda, this.fase);
   }
 
 }
